163897324afa8a65e2b9a4120508aee154d0e57b
Arif Rubayet <aruba055@uottawa.ca>
2021-11-29 23:46:04 -0500

    Create Expense Tracker

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,17 @@
+import Card from './Card';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItems.css';
+
+function ExpenseItems(props) {
+    return (
+      <Card className="expense-item">
+       <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+        </div>
+        <div className="expense-item__price">${props.amount}</div>
+      </Card>
+    );
+}
+
+export default ExpenseItems;

bc6385282c0eec1ce830d65e125ce84a409549e4
Arif Rubayet <aruba055@uottawa.ca>
2021-12-02 19:04:06 -0500

    Using One state for multipple task

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,11 +2,29 @@ import React, {useState} from "react";
 import './NewExpenseForm.css';
 
 const ExpenseForm=()=>{
-    const [enteredTitle, setenteredTitle]= useState('');
+/*     const [enteredTitle, setenteredTitle]= useState('');
     const [enteredAmount, setenteredAmount]= useState('');
-    const [enteredDate, setenteredDate]= useState('');
-    
+    const [enteredDate, setenteredDate]= useState(''); */
+
+    const [inputHandeler, setinputHandeler]= useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
+
+
     const titleHandeler= (event)=>{
+        setinputHandeler({...inputHandeler, enteredTitle: event.target.value})
+    }
+
+    const amountHandler= (event)=>{
+        setinputHandeler({...inputHandeler, enteredAmount: event.target.value})
+    }
+    const dateHandler= (event)=>{
+        setinputHandeler({...inputHandeler, enteredDate: event.target.value})
+    }
+    
+  /*   const titleHandeler= (event)=>{
         setenteredTitle(event.target.value)
     };
     const amountHandler= (event)=>{
@@ -14,7 +32,7 @@ const ExpenseForm=()=>{
     };
     const dateHandler = (event) =>{
         setenteredDate(event.target.value)
-    };
+    }; */
     return (<form>
         <div className='new-expense__controls'>
         <div className='new-expense__control'>

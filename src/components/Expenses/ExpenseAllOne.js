e11fe2d86647e976f347061489c3982101bf2e50
Arif Rubayet <aruba055@uottawa.ca>
2021-12-03 21:00:23 -0500

    Add items/expense dynamically

--- src/components/Expenses/ExpenseAllOne.js
+++ src/components/Expenses/ExpenseAllOne.js
@@ -13,31 +13,18 @@ const [enteredYear, setenteredYear]= useState('2020');
     setenteredYear(enteredFilterData); 
   }
 return (
-<Card className="expenses">
-  <ExpensesFilter selected={enteredYear} onFilterChange={getFilterChange} />
+  <Card className="expenses">
+    <ExpensesFilter selected={enteredYear} onFilterChange={getFilterChange} />
 
-<ExpenseItems
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
+    {props.items.map((expenses) => (
       <ExpenseItems
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
+        title={expenses.title}
+        amount={expenses.amount}
+        date={expenses.date}
       />
-      <ExpenseItems
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItems
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    
-</Card>
+    ))}
+
+  </Card>
 );
 }
 
